7da0bd594fb64adc33bedfff2ced957f8ca575c1
iamanshulkumar <anshulkm17@gmail.com>
2022-11-26 12:52:14 +0530

    done with exercise detail component

--- gym_exercise/src/pages/ExerciseDetail.js
+++ gym_exercise/src/pages/ExerciseDetail.js
@@ -16,7 +16,7 @@ const ExerciseDetail = () => {
 			const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
 			const youtubeSearchUrl = 'https://https://youtube-search-and-download.p.rapidapi.com'
 
-			const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/${id}`, exerciseOptions);
+			const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
 			setExerciseDetail(exerciseDetailData);
 
 		}
@@ -26,7 +26,7 @@ const ExerciseDetail = () => {
 
 	return (
 		<Box>
-			<Detail />
+			<Detail exerciseDetail={exerciseDetail} />
 			<ExerciseVideo />
 			<SimilarExercises />
 		</Box>
