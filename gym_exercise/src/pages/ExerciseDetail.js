5303936c97aa1a531292606c2728fd3f96e854d5
iamanshulkumar <anshulkm17@gmail.com>
2022-11-26 13:18:43 +0530

    Started working on similar exercise component

--- gym_exercise/src/pages/ExerciseDetail.js
+++ gym_exercise/src/pages/ExerciseDetail.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material'
 
-import { exerciseOptions, fetchData } from '../utils/fetchData';
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 import Detail from '../components/Detail';
 import ExerciseVideo from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
 	const [exerciseDetail, setExerciseDetail] = useState({});
+	const [exerciseVideos, setExerciseVideos] = useState([]);
 	const { id } = useParams();
 
 	useEffect(() => {
@@ -19,6 +20,8 @@ const ExerciseDetail = () => {
 			const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
 			setExerciseDetail(exerciseDetailData);
 
+			const exerciseVideoData = await fetchData(`${youtubeSearchUrl}/search?q=${exerciseDetailData.name}`, youtubeOptions);
+			setExerciseVideos(exerciseDetailData)
 		}
 
 		fetchExercisesData();
@@ -27,7 +30,7 @@ const ExerciseDetail = () => {
 	return (
 		<Box>
 			<Detail exerciseDetail={exerciseDetail} />
-			<ExerciseVideo />
+			<ExerciseVideo exerciseVideo={exerciseVideos} nmae={exerciseDetail.name} />
 			<SimilarExercises />
 		</Box>
 	)
