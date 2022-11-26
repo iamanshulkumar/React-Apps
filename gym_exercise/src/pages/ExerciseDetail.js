c9e93ce90d73fd1b3ea6510183ccbd19fea56207
iamanshulkumar <anshulkm17@gmail.com>
2022-11-26 14:06:17 +0530

    Debugging
    Having CORS issue

--- gym_exercise/src/pages/ExerciseDetail.js
+++ gym_exercise/src/pages/ExerciseDetail.js
@@ -10,6 +10,8 @@ import SimilarExercises from '../components/SimilarExercises';
 const ExerciseDetail = () => {
 	const [exerciseDetail, setExerciseDetail] = useState({});
 	const [exerciseVideos, setExerciseVideos] = useState([]);
+	const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+	const [equipmentExercises, setEquipmentExercises] = useState([]);
 	const { id } = useParams();
 
 	useEffect(() => {
@@ -21,7 +23,13 @@ const ExerciseDetail = () => {
 			setExerciseDetail(exerciseDetailData);
 
 			const exerciseVideoData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`, youtubeOptions);
-			setExerciseVideos(exerciseDetailData.contents)
+			setExerciseVideos(exerciseVideoData.contents)
+
+			const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
+			setTargetMuscleExercises(targetMuscleExercisesData);
+
+			const equipmentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions);
+			setEquipmentExercises(equipmentExercisesData);
 		}
 
 		fetchExercisesData();
@@ -31,7 +39,7 @@ const ExerciseDetail = () => {
 		<Box>
 			<Detail exerciseDetail={exerciseDetail} />
 			<ExerciseVideo exerciseVideos={exerciseVideos} nmae={exerciseDetail.name} />
-			<SimilarExercises />
+			<SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
 		</Box>
 	)
 }
