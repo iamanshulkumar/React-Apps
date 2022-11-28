10c47515a33d7969088cce0f65965cdf7500dca7
iamanshulkumar <anshulkm17@gmail.com>
2022-11-28 19:28:27 +0530

    Creating server

--- amazon_scraper_api/index.js
+++ amazon_scraper_api/index.js
@@ -4,10 +4,28 @@ const request = require('request-promise');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const apiKey = '08616b73d250906a5e37a7fd1a94cccd'; // scraper api key
+const baseUrl = `https://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
 app.use(express.json());
 
+// Welcome route
 app.get('/', (req, res) => {
-	res.send('welcome to Amazon scraper API.');
+	res.send('Welcome to Amazon scraper API.');
 }); 
 
-app.listen(PORT, () => console.log(`Server running at ${PORT}`));
+// GET Product Details
+app.get('/products/:productId', async (req, res) => {
+	const { productId } = req.params;
+  const { api_key } = req.query;
+
+	try {
+		const response = await request(`${bseUrl}&url=https://www.amazon.com/dp/${productId}`);
+
+		res.json(JSON.parse(response));
+	}	catch (error) {
+			res.json(error);
+	}
+});
+
+app.listen(PORT, () => console.log(`Server running on port => ${PORT}`));
